815e6d2fe59e2ee7ac6068d5cdd7625a93d784be
Vikms95 <vms1995@hotmail.com>
2022-05-01 11:52:51 +0200

    Setup components

---
+++ react-scrimba-1/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.css';
+import About from './components/About'
+import Info from './components/Info'
+import Interests from './components/Interests'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+
+      <About />
+      <Info />
+      <Interests />
+      <Footer /> 
+      
+    </>
+  );
+}
+
+export default App;
